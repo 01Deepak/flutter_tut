38af5c8872836d544319f7dfb4b51b284f83afad
Deepak Singh <deepak.singh@growthnatives.co>
2023-02-06 17:41:51 +0530

    stateless widgets

--- lib/main.dart
+++ lib/main.dart
@@ -2,45 +2,41 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      theme: ThemeData(
-          primarySwatch: Colors.purple,
-          accentColor: Colors.cyan,
-          textTheme: TextTheme(
-              bodySmall: TextStyle(
-            fontFamily: 'IndieFlower',
-            fontSize: 30,
-          ))),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'my first app in flutter',
-          ),
-        ),
-        body: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'hello deepak   dgjds;gszdn;fndsgjknxdfgnx;dfgn;xfdjn;j',
-                  style: TextStyle(fontFamily: 'IndieFlower'),
-                ),
-                TextField(
-                  decoration:
-                      InputDecoration(label: Text("name"), hintText: "Main"),
-                )
-              ],
-            ),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            print("floating action button clicked");
-          },
-          child: Text('click'),
+    MaterialApp(home: Home()),
+  );
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'my first app in flutter',
         ),
       ),
-    ),
-  );
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'hello deepak',
+                style: TextStyle(fontFamily: 'IndieFlower'),
+              ),
+              Text('hello Akash')
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print("floating action button clicked");
+        },
+        child: Text('click'),
+      ),
+    );
+  }
 }
