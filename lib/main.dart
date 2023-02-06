715c742c88c150a0bd8b6c44c143081063b6c3a9
Deepak Singh <deepak.singh@growthnatives.co>
2023-02-06 14:05:14 +0530

    change app bar background color

--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,26 @@ void main() {
     MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('my first app in flutter'),
+          title: Text(
+            'my first app in flutter',
+          ),
+          backgroundColor: Colors.red[500],
         ),
         body: Center(
-          child: Text('hello deepak'),
+          child: Text(
+            'hello deepak',
+            style: TextStyle(
+              color: Colors.red[500],
+              fontSize: 20,
+            ),
+          ),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
             print("floating action button clicked");
           },
           child: Text('click'),
+          backgroundColor: Colors.red[500],
         ),
       ),
     ),
